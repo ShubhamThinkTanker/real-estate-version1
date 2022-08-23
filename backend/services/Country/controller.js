2b75c7a6e67b64a781c003bcc6547831253a0e2c
shubham Vaishnav <shubham.thinktanker@gmail.com>
2022-08-23 12:24:32 +0530

    create state and city api

--- backend/services/Country/controller.js
+++ backend/services/Country/controller.js
@@ -1,5 +1,7 @@
 // const EstateStructureServices = require('./service')
 const countryModel = require('./countryModel')
+const stateModel = require('./stateModel')
+const cityModel = require('./Citymodel')
 const { Country, State, City } = require("country-state-city")
 const { commonResponse } = require("../../helper");
 module.exports = {
@@ -26,15 +28,50 @@ module.exports = {
 
   GetAllState: async (req, res, next) => {
     try {
-      let allCountry = await countryModel.find()
-      let countryName = allCountry.map(index => index.name)
+
+      let countryname = req.query.countryname || "";
+
+      let FindCountrycode = await countryModel.find({ name: countryname })
+      let getCountryCode = FindCountrycode.map(index => index.countryCode)
+
+      let allState = await stateModel.find({ countryCode: getCountryCode })
+
+      let stateName = allState.map(index => index.name)
 
 
-      if (countryName) {
-        return commonResponse.success(res, 200, 'Get all countrys', countryName);
+
+      if (stateName) {
+        return commonResponse.success(res, 200, 'Get all State', stateName);
       } else {
         return commonResponse.customErrorResponse(res, 422, 'Something went wrong');
       }
+    } catch (error) {
+      console.log(error);
+    }
+  },
+
+  GetAllCity: async (req, res, next) => {
+    try {
+
+      let statename = req.query.statename || "";
+      let FindCountrycode = await stateModel.find({ name: statename })
+
+      let getisoCode = FindCountrycode.map(index => index.isoCode)
+      let getCountry = FindCountrycode.map(index => index.countryCode)
+
+      let allCountry = await cityModel.find({
+        stateCode: getisoCode,
+        countryCode: getCountry
+      })
+
+      let cityName = allCountry.map(index => index.name)
+
+      if (cityName) {
+        return commonResponse.success(res, 200, 'Get all State', cityName);
+      } else {
+        return commonResponse.customErrorResponse(res, 422, 'Something went wrong');
+      }
+
     } catch (error) {
       console.log(error);
     }
--- backend/services/Country/routes.js
+++ backend/services/Country/routes.js
@@ -1,8 +1,18 @@
 const router = require('express').Router();
 const controller = require('./controller')
 
-router.post(
+router.get(
     '/get_all_country',
     controller.GetAllCountry);
+
+
+router.get(
+    '/get_all_state',
+    controller.GetAllState);
+
+
+router.get(
+    '/get_all_city',
+    controller.GetAllCity);
 
 module.exports = router;
